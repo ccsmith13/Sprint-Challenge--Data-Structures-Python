c0454eb7cdccded12e825f92185af8e78d52b9a4
Christine Smith <christinecsmith1@gmail.com>
2020-09-27 18:24:51 -0700

    ring_buffer mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.ring = []
+        self.oldest_value_index = 0 
 
     def append(self, item):
-        pass
-
+        ring_length = len(self.ring)
+        if ring_length == self.capacity: 
+            self.ring[self.oldest_value_index] = item
+            if self.oldest_value_index + 1 == self.capacity: 
+                self.oldest_value_index = 0
+            else: 
+                self.oldest_value_index += 1
+        else: 
+            self.ring.append(item)
+ 
     def get(self):
-        pass
+        return self.ring
